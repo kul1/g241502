2837a3d5b4074b207079192f2b15621a8ae595a3
P Kul <1.0@kul.asia>
2017-04-05 06:23:45 +0700

    Fixed heroku error mindapp_controller line 50 error update_attribute

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  post ':controller(/:action(/:id))(.:format)'
+  get ':controller(/:action(/:id))(.:format)'
+  get '/logout' => 'sessions#destroy', :as => 'logout'
+  get '/auth/failure' => 'sessions#failure'
+  post '/auth/:provider/callback' => 'sessions#create'
+  resources :sessions
+  resources :identities
+  root :to => 'mindapp#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
